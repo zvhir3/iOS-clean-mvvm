6ca0048f6c345ace4749d3943d29b8f775a700c9
zvhir <zahiruddin.developer@gmail.com>
2025-08-05 17:50:53 +0800

    Initial Commit

---
+++ plasma/plasmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  plasmaApp.swift
+//  plasma
+//
+//  Created by NightfallNest on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct plasmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
